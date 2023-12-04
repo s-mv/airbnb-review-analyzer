ffeeb3022265103fa89341a9f48b640a0aec1273
Shreerang Vaidya <shreerangvaidya28@gmail.com>
2023-12-04 14:39:37 +0530

    selenium

---
+++ airbnb.py
@@ -0,0 +1,43 @@
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+REVIEWS_CONTAINER = (
+    ".di536pa.atm_ks_zryt35.atm_mk_h2mmj6.atm_26_1hbpp16"
+    ".atm_vy_auwlz6.atm_j3_auwlz6.atm_iy_1osqo2v.atm_9s_1txwivl"
+    ".atm_ar_1bp4okc.atm_70_1i7rd3n.atm_6a_1hl1osj.atm_6c_1hl1osj"
+    ".atm_y_1bbsqr7.atm_16_kb7nvz.atm_12_1hrf63d.atm_1c_m2duij"
+    ".atm_ks_15vqwwr__1yj3dog.atm_kd_glywfm_pfnrn2.atm_vy_1osqo2v__oggzyc"
+    ".atm_5j_1hl1osj__oggzyc.atm_1c_w5iguh__p88qr9.atm_j3_6tyhld__oggzyc.dclclpo"
+)
+
+REVIEW_ITEM = ".ll4r2nl.atm_kd_pg2kvz_1bqn0at"
+
+
+def strip_reviews_link(link: str) -> str:
+    url = urlparse(link)
+    if "airbnb" not in url.netloc or "/rooms/" not in url.path:
+        return None
+
+    return f"{url.scheme}://{url.netloc}{url.path}/reviews"
+
+
+def get_reviews(review_link: str):
+    chrome_options = webdriver.chrome.options.Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    chrome_options.add_argument("--headless")
+    container = None
+    try:
+        driver.get(review_link)
+        container = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, REVIEWS_CONTAINER))
+        )
+    finally:
+        driver.quit()
+
+    return container
--- analyze.py
+++ analyze.py
@@ -1,2 +1,13 @@
+import airbnb
 from bs4 import BeautifulSoup
+import requests
 
+example_link = "https://www.airbnb.co.in/rooms/753489119578553975?check_in=2023-12-15&check_out=2023-12-17&guests=1&adults=6&s=67&unique_share_id=e0a8ea9a-382b-4b9f-af19-34359fce4511"
+
+# print("Enter link")
+# input_link = input()
+
+link = airbnb.strip_reviews_link(example_link)
+content = airbnb.get_reviews(link)
+
+print(content)
